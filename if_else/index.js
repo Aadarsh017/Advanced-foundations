8ff1f6346be006626c5fb214ad3727596ddd2917
Aadarsh017 <maddheshiyaaadarsh9@gmail.com>
2026-09-21 18:51:09 +0530

    switch statement

--- if_else/index.js
+++ if_else/index.js
@@ -1,5 +1,16 @@
-const exerciseTime = 70
-const msg = exerciseTime < 30 ? "you need to try hard" 
-: exerciseTime < 60 ? "doing good!"
-: "excllent!" 
-console.log(msg);
+// const exerciseTime = 70
+// const msg = exerciseTime < 30 ? "you need to try hard"
+//     : exerciseTime < 60 ? "doing good!"
+//     : "excllent!"
+// console.log(msg);
+
+const playerGuess = 6
+const correctAnswer = 4
+
+let msg = playerGuess > correctAnswer ? "too high"
+     : playerGuess < correctAnswer ? "too low":
+     " Exactly right"
+    
+     
+
+    console.log(msg);
