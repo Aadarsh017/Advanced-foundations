7d2c98db12f4b6c266c58a76d02288640776300b
Aadarsh017 <maddheshiyaaadarsh9@gmail.com>
2026-09-23 21:07:32 +0530

    using funstions and settimeout

---
+++ switch statements/index.js
@@ -0,0 +1,69 @@
+// function selectItem(item) {
+//     let price = ""
+
+//     switch (item) {
+//         case 'coffee':
+//             price = 3
+//             break;
+
+//         case 'sandwiches':
+//             price = 5
+//             break;
+
+//         case 'Salad':
+//             price = 4
+//             break;
+
+//         case 'Lemon cake ':
+//             price = 9
+//             break;
+
+//         default:
+//             return `Sorry , we don't sell ${item}`
+
+//     }
+//     return `you select ${item}. that will be $${price}`
+// }
+
+// console.log(selectItem('bread'));
+
+// ________________________________________________________________________________________
+
+// const DreamHoliday = {
+//     distination: 'kasmir',
+//     acitivity: "sit side of a water fall and talk to our friends ",
+//     accommodation: "camp",
+//     companion: "Raunak and aditya "
+// }
+
+
+// const{ distination,acitivity,accommodation,companion } = DreamHoliday
+
+
+// console.log(`"I would love to go ${distination}
+//     ,${acitivity} and than go to our ${accommodation}
+//      or hang out with ${companion} "`);
+
+
+// ____________________________________________________________________________________________________
+
+// function traficLight(light) {
+//     console.log(light);
+//     setTimeout(traficLight , 3000 , "🤢")
+    
+// }
+
+// traficLight("🔴")
+
+`the answer is ${x}  of course ! if u got right
+     , give yourself ${y} point`
+
+
+
+
+function answer() {
+    console.log(`the answer is ${x}  of course ! if u got right
+     , give yourself ${y} point`);
+    
+}
+console.log('What is the capital of peru');
