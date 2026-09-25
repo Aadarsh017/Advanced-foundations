36476a6ac302925e6a7809ad794686f9da76b2ba
Aadarsh017 <maddheshiyaaadarsh9@gmail.com>
2026-09-25 15:41:28 +0530

    m

--- constructer/index.js
+++ constructer/index.js
@@ -1,2 +1,38 @@
-const date  = new Date()
-console.log(date.getFullYear());
+// const date  = new Date()
+// // console.log(date.getFullYear());
+// console.log(date);
+
+// __________________________________________________
+
+// error contrutore
+
+// function checkusername(name) {
+//     if (name) {
+//         console.log(name);
+        
+//     }
+//     else{
+//         console.log(new Error("this is empty"));
+//         // we also use throw but when we use throw than nextall the line does not work or print
+//         // throw new Error("this is empty");
+//         // console.log("xyz");
+        
+//     }
+// }
+// checkusername()
+
+// _________________________________________________________________
+
+// const x = 2_9900_44_333_049983n // bigint
+// // console.log(typeof x);
+
+
+// ________________________________________________________________
+
+// challange
+
+
+
+
+
+
