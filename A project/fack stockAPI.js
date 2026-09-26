08d4da07000e76eca957381fcb3f702d36647c3d
Aadarsh017 <maddheshiyaaadarsh9@gmail.com>
2026-09-27 01:48:13 +0530

    A project

--- A project/fack stockAPI.js
+++
@@ -1,9 +0,0 @@
-function getStockData() {
-    return {
-        name: 'QsohkAI',
-        Symbol: 'QTA',
-        price: (Math.random() * 3).tofixed(2),
-        Time: new Time()
-    }
-
-}
--- A project/index.js
+++
@@ -1,8 +0,0 @@
-function renderStockTracker(stockData) {
-    const stockDisplayName = document.getElementById('name')
-    const stockDisplaySymbol = document.getElementById('symbol')
-    const stockDisplayPrice = document.getElementById('price')
-    const stockDisplaypriceIcon = document.getElementById('price-icon')
-    const stockDisplaypriceTime = document.getElementById('time')
-
-}
